ec01f25cb9e9bc7659396554f7e12d3ba3f58e9c
Gabriel Lucena <gabrielml371@gmail.com>
2021-10-18 14:52:32 -0300

    project start

---
+++ android/app/src/main/kotlin/com/example/rnkr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rnkr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'rnkr',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
